5e27fed653bbbf91d7bb0fc079125221d9ba9576
Daki Madinina <daki.123456@gmail.com>
2017-08-29 13:04:33 -0400

    +ajout de cas concret

--- app/src/main/java/aw3s0me/tuts/drill_java/CasConcret.java
+++ app/src/main/java/aw3s0me/tuts/drill_java/CasConcret.java
@@ -1,5 +1,9 @@
 package aw3s0me.tuts.drill_java;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by RIAMM on 25/08/2017.
  */
@@ -16,7 +20,6 @@ public class CasConcret {
 
         while(montant_courant < plafond)
         {
-            //System.out.println("Avant calcul : " + montant_courant);
             i++;
             System.out.println("Avant calcul année "+i+" : " + montant_courant);
             montant_courant += montant_courant * interet_normalise;
@@ -25,5 +28,47 @@ public class CasConcret {
         }
 
         return i;
+    }
+
+    public static float calcul_valeur_medianne(int[] valeurs)
+    {
+        Arrays.sort(valeurs);
+
+        if(valeurs.length % 2 != 0)
+        {
+            return valeurs[(valeurs.length-1)/2];
+        }
+        else
+        {
+            return (float)(valeurs[(valeurs.length/2)-1] + valeurs[(valeurs.length/2)])/2;
+        }
+
+    }
+
+    public static HashMap<Character,Float> distribution_probabilite(char[] caracteres)
+    {
+
+        HashMap<Character,Integer> dist = new HashMap<>();
+
+        for(char c : caracteres)
+        {
+            if(dist.containsKey(c))
+            {
+                dist.put(c,dist.get(c)+1);
+            }
+            else
+            {
+                dist.put(c,1);
+            }
+        }
+
+        HashMap<Character,Float> probDist = new HashMap<>();
+
+        for(Map.Entry<Character,Integer> entry: dist.entrySet())
+        {
+            probDist.put(entry.getKey(),(float)entry.getValue()/caracteres.length);
+        }
+
+        return probDist;
     }
 }
--- app/src/test/java/aw3s0me/tuts/drill_java/CasConcretTest.java
+++ app/src/test/java/aw3s0me/tuts/drill_java/CasConcretTest.java
@@ -2,6 +2,8 @@ package aw3s0me.tuts.drill_java;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.isA;
 /**
@@ -22,10 +24,45 @@ public class CasConcretTest {
         int annees3 = CasConcret.annees_avant_plafond(6,22750,62700);
         assertEquals(18,annees3);
 
-
         System.out.print(annees3);
+    }
+
+    @Test
+    public void test_calcul_valeur_medianne()
+    {
+        int[] tab1 = {1,2,3,4,5};
+        assertEquals(3,CasConcret.calcul_valeur_medianne(tab1),0);
+
+        int[] tab2 = {1,2,3,4,5,6};
+        assertEquals(3.5f,CasConcret.calcul_valeur_medianne(tab2),0);
+
+        int[] tab3 = {4,6,3,1,2,5};
+        assertEquals(3.5f,CasConcret.calcul_valeur_medianne(tab3),0);
+
+        int[] tab4 = {278,9861,6945,1,8613};
+        assertEquals(6945f,CasConcret.calcul_valeur_medianne(tab4),0);
+
+        int[] tab5 = {278,9861,6945,1,8613,94856};
+        assertEquals(7779f,CasConcret.calcul_valeur_medianne(tab5),0);
+    }
+
+    @Test
+    public void test_distribution_probabilite()
+    {
+        char[] caracs = {'A','B','C','D','E','Z','A','A','D','E','Z','B','B','E','A','B'};
+
+        assertThat(CasConcret.distribution_probabilite(caracs) , isA(HashMap.class));
+
+        HashMap<Character,Float> probs =  CasConcret.distribution_probabilite(caracs);
 
 
+
+        assertEquals(0.25f,probs.get('A'),0);
+        assertEquals(0.25f,probs.get('B'),0);
+        assertEquals(0.0625f,probs.get('C'),0);
+        assertEquals(0.125f,probs.get('D'),0);
+        assertEquals(0.1875f,probs.get('E'),0);
+        assertEquals(0.125f,probs.get('Z'),0);
     }
 
 }
